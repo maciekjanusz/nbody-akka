3ba000bd13c4623e012d6a179311e20c6cbadb4d
Maciej Janusz <maciej.st.janusz@gmail.com>
2015-12-29 20:36:20 +0100

    accumulation proto

--- src/main/scala/nbody/Body.scala
+++ src/main/scala/nbody/Body.scala
@@ -10,9 +10,9 @@ sealed class Body[S <: BodyState : ClassTag](n: Long, tMax: Long, initialState: 
   def peers = n - 1
 
   var currentState = initialState
-  val bodies = context.actorSelection("../*") // siblings
-  val peerStates = StateSet.empty[S]
-  val futureStates = StateSet.empty[S]
+  //  val peerStates = StateSet.empty[S]
+  var processedStates = 0
+  var futureStates = 0 //StateSet.empty[S]
 
   override def receive: Receive = {
     case Start =>
@@ -24,37 +24,35 @@ sealed class Body[S <: BodyState : ClassTag](n: Long, tMax: Long, initialState: 
       }
   }
 
+
   def receiveState(state: S): Unit = {
     if (state.t == currentState.t) {
-      peerStates += state
+      processedStates += 1
     } else {
-      futureStates += state
+      futureStates += 1
     }
 
     var t = state.t
 
-    while (peerStates.size == peers) {
+    while (processedStates == peers) {
       if(named("body-0")) print("#")
-      // calculate new state
-      val nextTime = t + 1
-      currentState = nextState(currentState, peerStates)
-      peerStates.clear()
+      processedStates = 0
+      val tPlus = t + 1
+      currentState = nextState(currentState, Seq.empty[S])
 
-      if (nextTime == tMax) {
-        if(named("body-0")) println()
+      if (tPlus == tMax) {
         context.parent ! currentState
       } else {
         broadcastState(currentState)
-        peerStates ++= futureStates
-        futureStates.clear()
-
+        processedStates += futureStates
+        futureStates = 0
         t += 1
       }
     }
   }
 
   def broadcastState(state: S): Unit = {
-    bodies ! state
+    context.actorSelection("../*") ! state
   }
 
   def named(name: String) = self.path.name equals name
